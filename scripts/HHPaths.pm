df9de66d23d2bd77e14c0c4d5f6bd398f2a1dfb4
Dmitriy Samborskiy <samborsky_d@yahoo.com>
2015-03-26 19:14:08 +0300

    v.2.0.3

--- scripts/HHPaths.pm
+++ scripts/HHPaths.pm
@@ -1,6 +1,6 @@
 # Align.pm 
 
-# HHsuite version 2.0.1 (January 2012)
+# HHsuite version 2.0.3 (January 2012)
 # (c) J. Soeding, A. Hauser 2012
 #
 
@@ -18,16 +18,22 @@ our @ISA     = qw(Exporter);
 our @EXPORT  = qw($hhlib $hhdata $hhbin $hhscripts $execdir $datadir $ncbidir $dummydb $pdbdir $dsspdir $dssp $cs_lib $context_lib);
 
 ##############################################################################################
-# PLEASE COMPLETE THE PATHS TO PSIPRED AND OLD-STYLE BLAST (NOT BLAST+) (NEEDED FOR PSIPRED) 
-our $execdir = ".../psipred/bin";         # path to PSIPRED V2 binaries
-our $datadir = ".../psipred/data";        # path to PSIPRED V2 data files
-our $ncbidir = ".../blast/bin";           # path to NCBI binaries (for PSIPRED in addss.pl)
+# PLEASE COMPLETE THE PATHS ... TO PSIPRED AND OLD-STYLE BLAST (NOT BLAST+) (NEEDED FOR PSIPRED) 
+#our $execdir = ".../psipred/bin";         # path to PSIPRED V2 binaries
+#our $datadir = ".../psipred/data";        # path to PSIPRED V2 data files
+#our $ncbidir = ".../blast/bin";           # path to NCBI binaries (for PSIPRED in addss.pl)
+our $execdir = "/cluster/bioprogs/psipred/bin";         # Where the PSIPRED V2 programs have been installed
+our $datadir = "/cluster/bioprogs/psipred/data";        # Where the PSIPRED V2 data files have been installed
+our $ncbidir = "/cluster/bioprogs/blast/bin";           # Where the NCBI programs have been installed (for PSIPRED in addss.pl)
 
 ##############################################################################################
-# PLEASE COMPLETE THE PATHS TO YOUR LOCAL PDB FILES, DSSP FILES ETC.
-our $pdbdir  =  ".../pdb/all";            # where are the pdb files? Used in hhmakemodel.pl.
-our $dsspdir =  ".../dssp/data";          # where are the dssp files? Used in addss.pl.
-our $dssp    =  ".../dssp/bin/dsspcmbi";  # where is the dssp binary? Used in addss.pl.
+# PLEASE COMPLETE THE PATHS ... TO YOUR LOCAL PDB FILES, DSSP FILES ETC.
+#our $pdbdir  =  ".../pdb/all";            # where are the pdb files? Used in hhmakemodel.pl.
+#our $dsspdir =  ".../dssp/data";          # where are the dssp files? Used in addss.pl.
+#our $dssp    =  ".../dssp/bin/dsspcmbi";  # where is the dssp binary? Used in addss.pl.
+our $pdbdir  =  "/cluster/databases/pdb/all";            # where are the pdb files? Used in hhmakemodel.pl
+our $dsspdir =  "/cluster/databases/dssp/data";          # where are the dssp files? Used in addss.pl
+our $dssp    =  "/cluster/databases/dssp/bin/dsspcmbi";  # where is the dssp binary? Used in addss.pl
 ##############################################################################################
 
 # The lines below probably do not need to be changed
@@ -35,7 +41,7 @@ our $dssp    =  ".../dssp/bin/dsspcmbi";  # where is the dssp binary? Used in ad
 # Setting paths for hh-suite perl scripts
 our $hhlib    = $ENV{"HHLIB"};     # main hh-suite directory
 our $hhdata   = $hhlib."/data";    # path to data directory for hhblits, example files
-our $hhbin    = $hhlib."/bin";     # path to cstranslate
+our $hhbin    = $hhlib."/bin";     # path to cstranslate (path to hhsearch, hhblits etc. should be in $PATH)
 our $hhscripts= $hhlib."/scripts"; # path to hh perl scripts (addss.pl, reformat.pl, hhblitsdb.pl etc.)
 our $dummydb  = $hhdata."/do_not_delete"; # Name of dummy blast db for PSIPRED (single sequence formatted with NCBI formatdb)
 
